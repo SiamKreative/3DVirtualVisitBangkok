4d64e81eb8203b52b1d68b84901ab9d7e5b2bf54
Julien Vernet <julien@vernet.me>
2017-08-03 20:35:22 +0700

    Create link to download thumbnail

--- admin/js/vvb-admin.js
+++ admin/js/vvb-admin.js
@@ -1,32 +1,30 @@
-(function( $ ) {
+(function ($) {
 	'use strict';
 
-	/**
-	 * All of the code for your admin-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
+	$(function () {
+		var id;
+		var input = $('[data-name="matterportcom_id"] :input');
+		var description = $('[data-name="matterportcom_id"] .description');
 
-})( jQuery );
+		function downloadThumbLink() {
+			id = input.val();
+			if (description.length && input.length && id) {
+				description.find('a').attr({
+					href: 'https://my.matterport.com/api/v1/player/models/' + id + '/thumb',
+					download: true
+				});
+			}
+		}
+
+		// When input changes
+		input.on('input', function (event) {
+			event.preventDefault();
+			downloadThumbLink();
+		});
+
+		// When page loads
+		downloadThumbLink();
+
+	});
+
+})(jQuery);
